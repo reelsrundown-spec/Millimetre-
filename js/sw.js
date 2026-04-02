c41d6abc8e79f30c62b6c358e62401d3d051e799
Thalapath <reelsrundown@gmail.com>
2026-04-02 21:01:10 +0530

    Create sw.js

---
+++ js/sw.js
@@ -0,0 +1,11 @@
+const cacheName = 'snl-v1';
+const assets = ['./', './index.html', './loading.html', './home.html', './game.html', './style.css', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+
