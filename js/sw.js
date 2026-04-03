30441668506bc7e17aa924b27b98645cf428c7d0
Thalapath <reelsrundown@gmail.com>
2026-04-03 23:05:59 +0530

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -9,3 +9,12 @@ self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
 
+<script>
+    if ('serviceWorker' in navigator) {
+        // '../js/sw.js' ennalla, root-il aanel 'sw.js' ennu mathram mathi
+        navigator.serviceWorker.register('sw.js')
+        .then(() => console.log("Service Worker Registered"))
+        .catch(err => console.log("SW Register Error:", err));
+    }
+</script>
+
